0919f02f6c063f10ada46a418797abdaa899c89d
Uzema <uzema_2k16@mail.ru>
2026-03-03 00:36:48 +0300

    typo

--- tasks/nikolaev_d_block_linear_image_filtering_seq/common/include/common.hpp
+++ tasks/nikolaev_d_block_linear_image_filtering_seq/common/include/common.hpp
@@ -9,7 +9,7 @@
 
 namespace nikolaev_d_block_linear_image_filtering {
 
-using InType = std::tuple<int, int, std::vector<uint8_t>>;  // img width, img heigt, pixel data
+using InType = std::tuple<int, int, std::vector<uint8_t>>;  // img width, img height, pixel data
 using OutType = std::vector<uint8_t>;
 using TestType = std::tuple<InType, OutType, std::string>;
 using BaseTask = ppc::task::Task<InType, OutType>;
