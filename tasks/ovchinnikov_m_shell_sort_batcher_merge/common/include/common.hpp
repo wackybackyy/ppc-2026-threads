ab18d3723ed82fd05945e7e3c88b5dde62ea3427
1Dura <BeSTaR_X@mail.ru>
2026-04-07 13:44:14 +0300

    common dir name fix

--- tasks/ovchinnikov_m_shell_sort_batcher_merge/common/include/common.hpp
+++ tasks/ovchinnikov_m_shell_sort_batcher_merge/common/include/common.hpp
@@ -6,11 +6,11 @@
 
 #include "task/include/task.hpp"
 
-namespace ovchinnikov_m_shell_sort_batcher_merge_seq {
+namespace ovchinnikov_m_shell_sort_batcher_merge {
 
 using InType = std::vector<int>;
 using OutType = std::vector<int>;
 using TestType = std::tuple<InType, OutType, std::string>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
-}  // namespace ovchinnikov_m_shell_sort_batcher_merge_seq
+}  // namespace ovchinnikov_m_shell_sort_batcher_merge
